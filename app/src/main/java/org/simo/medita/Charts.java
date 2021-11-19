4a8d803d17eb5d58afdd51753b2fda9be29cc139
Igna <ipaso@idesoftbcn.com>
2021-11-19 19:48:27 +0100

    Actualización de las nuevas tareas de la lista

--- app/src/main/java/org/simo/medita/Charts.java
+++ app/src/main/java/org/simo/medita/Charts.java
@@ -23,6 +23,7 @@ public class Charts  extends Activity {
     protected SlidingMenu menu_lateral;
     protected ImageView menu;
     protected BarChart barchart1;
+    protected BarChart barchart2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +35,12 @@ public class Charts  extends Activity {
         prefs = getSharedPreferences(getString(R.string.sharedpref_name), Context.MODE_PRIVATE);
 
         barchart1 = findViewById(R.id.barchart1);
+        barchart2 = findViewById(R.id.barchart2);
 
-        showBarChart();
+        showBarChart1();
+        showBarChart2();
     }
-    private void showBarChart(){
+    private void showBarChart1(){
         ArrayList<Double> valueList = new ArrayList<Double>();
         ArrayList<BarEntry> entries = new ArrayList<>();
         String title = "Title";
@@ -58,5 +61,27 @@ public class Charts  extends Activity {
         BarData data = new BarData(barDataSet);
         barchart1.setData(data);
         barchart1.invalidate();
+    }
+    private void showBarChart2(){
+        ArrayList<Double> valueList = new ArrayList<Double>();
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        String title = "Title";
+
+        //input data
+        for(int i = 0; i < 6; i++){
+            valueList.add(i * 100.1);
+        }
+
+        //fit the data into a bar
+        for (int i = 0; i < valueList.size(); i++) {
+            BarEntry barEntry = new BarEntry(i, valueList.get(i).floatValue());
+            entries.add(barEntry);
+        }
+
+        BarDataSet barDataSet = new BarDataSet(entries, title);
+
+        BarData data = new BarData(barDataSet);
+        barchart2.setData(data);
+        barchart2.invalidate();
     }
 }
